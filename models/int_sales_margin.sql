3a596b45692868826f8cff145b3b0fca4a5a0b22
Antoine KIEFFER <antoine.kieffer.pro@gmail.com>
2025-11-05 11:46:32 +0000

    modify int_sales_margin

--- models/int_sales_margin.sql
+++ models/int_sales_margin.sql
@@ -1,6 +1,10 @@
+WITH sales_purchase_cost AS(
 SELECT*,
 ROUND (purchase_price*quantity,2) AS purchase_cost,
-ROUND (revenue - purchase_price*quantity,2) AS margin
 FROM {{ ref('stg_gz_raw_data__sales')}}
 JOIN {{ ref('stg_gz_raw_data__product')}}
 USING (products_id)
+)
+SELECT*,
+{{margin_percent('revenue','purchase_cost')}} AS margin_percent
+FROM sales_purchase_cost
